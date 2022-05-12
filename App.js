017b78fb0ae5f009daef83ece1c442101fcfb351
facuvila <facundovilamowski@gmail.com>
2022-05-11 23:44:26 -0300

    Scan OK

--- App.js
+++ App.js
@@ -1,26 +1,87 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { Text, View, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {  BarCodeScanner  } from 'expo-barcode-scanner';
+import { Dimensions } from 'react-native';
 
-function HomeScreen() {
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+
+function HomeScreen({ navigation, route }) {
+  React.useEffect(() => {}, [route.params?.post]);
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
+      <Text>LEAF CASH</Text>
+      <Button
+        title="ESCANEAR"
+        onPress={() => navigation.navigate('Scan')}
+      />
+      <Text> {route.params?.post} </Text>
     </View>
   );
+}
+
+function ScanScreen({ navigation }) {
+    const [hasPermission, setHasPermission] = useState(null);
+
+    // Pide permiso de la cámara
+    const askForCameraPermission = () => {
+        (async () => {
+            const { status } = await BarCodeScanner.requestPermissionsAsync();
+            setHasPermission(status === 'granted');
+        })();
+    };
+
+    useEffect(() => {
+        askForCameraPermission();
+    }, []);
+
+    // Lo que ocurre al detectar un código QR
+    const handleBarCodeScanned = ({ data }) => {
+        navigation.navigate({
+          name: 'Home',
+          params: { post: data },
+          merge: true,
+        })
+    };
+
+    // Devuelve las pantallas en base a los permisos concedidos
+    if (hasPermission === null) {
+        return (
+            <View>
+                <Text>Por favor, permití el acceso a la cámara.</Text>
+            </View>);
+    }
+    if (hasPermission === false) {
+        return (
+            <View>
+                <Text style={{ margin: 10 }}>Sin acceso a la cámara.</Text>
+                <Button title={'Permitir acceso a la cámara.'} onPress={() => askForCameraPermission()} />
+            </View>);
+    }
+
+    // Devuelve la View del Scanner
+    return (
+        <View>
+            <BarCodeScanner
+                onBarCodeScanned={handleBarCodeScanned}
+                style={{ height: windowHeight, width: windowWidth }} />
+        </View>
+    );
 }
 
 const Stack = createNativeStackNavigator();
 
 function App() {
+  // Return the View
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Scan" component={ScanScreen} />
+    </Stack.Navigator>
+  </NavigationContainer>
   );
 }
 
