4303444af24355c7365a528697e99a61302d293f
FACUNDO VILAMOVSKI <46208365@ort>
2022-05-23 10:28:03 -0300

    Added Axios GET

--- Screens/Home.js
+++ Screens/Home.js
@@ -1,22 +1,25 @@
 import React, {useState, useEffect} from 'react';
 import { View, Text, Button } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+const axios = require('axios');
 
 function Home({ result }) {
     const [isLoading, setLoading] = useState(true);
-    const [data, setData] = useState([]);
+    const [balance, setBalance] = useState([]);
     const navigation = useNavigation();
-    useEffect(() => {
-        fetch('https://leafcash.herokuapp.com/getUserData?id=michuturquie&campo=balance')
-          .then((response) => response.json())
-          .then((json) => setData(json))
-          .finally(() => setLoading(false))
-      }, []);
+    const loggedUser = 'facuvila';    
+
+    axios.get('https://leafcash.herokuapp.com/getUserData', { params: { id: loggedUser, campo:'balance' } })
+    .then(function (resp) {
+        setBalance(resp.data[0].balance);
+        setLoading(false);
+    });
+    
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             {isLoading ? <Text>Loading...</Text> : 
                 <>
-                <Text>LEAF CASH</Text><Text>${data[0].balance}</Text>
+                <Text>LEAF CASH</Text><Text>${balance}</Text>
                 <Button
                     title="ENVIAR"
                     onPress={() => navigation.navigate('Target')} />
