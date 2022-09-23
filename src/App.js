dd1862312687dcde5d4e034e8576f6e491f000a9
Android Studio <devandroid@ort>
2022-09-23 09:58:11 -0300

    Abanzado

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NativeBaseProvider } from 'native-base';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import Scan from './Screens/Common/Scan';
 import Home from './Screens/Common/Home';
@@ -16,24 +17,33 @@ import Plant from './Screens/Common/Plant';
 
 import StoreProvider from './store/StoreProvider';
 
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator()
+const Tab = createBottomTabNavigator()
+
+const HomeStack = () => {
+    return (
+        <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="Home" component={Home} />
+            <Stack.Screen name="Scan" component={Scan} />
+            <Stack.Screen name="Target" component={Target} />
+            <Stack.Screen name="Amount" component={Amount} />
+            <Stack.Screen name="Confirm" component={Confirm} />
+            <Stack.Screen name="QR" component={QR} />
+        </Stack.Navigator>
+    )
+}
 
 export default function App() {
     return (
         <NativeBaseProvider>
             <StoreProvider>
                 <NavigationContainer>
-                    <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
-                        <Stack.Screen name="Login" component={Login} />
-                        <Stack.Screen name="Signup" component={Signup} />
-                        <Stack.Screen name="Home" component={Home} />
-                        <Stack.Screen name="Scan" component={Scan} />
-                        <Stack.Screen name="Target" component={Target} />
-                        <Stack.Screen name="Amount" component={Amount} />
-                        <Stack.Screen name="Confirm" component={Confirm} />
-                        <Stack.Screen name="QR" component={QR} />
-                        <Stack.Screen name="Plant" component={Plant} />
-                    </Stack.Navigator>
+                <Stack.Screen name="Login" component={Login} />
+            <Stack.Screen name="Signup" component={Signup} />
+                    <Tab.Navigator>
+                        <Tab.Screen name="Home" component={HomeStack} />
+                        <Tab.Screen name="Plant" component={Plant} />
+                    </Tab.Navigator>
                 </NavigationContainer> 
             </StoreProvider>
         </NativeBaseProvider>  
